9caa4c0fe617b8f1b433d04baa5b041926dfd839
abdoneverland <ch.abdel.news@gmail.com>
2023-07-21 20:00:03 +0100

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort
+ *
+ * @array: The array
+ * @size: Number
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, t, c;
+	int m;
+
+	for (i = 0; i < size; i++)
+	{
+		m = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[m])
+			{
+				m = j;
+			}
+		}
+		if (m != i)
+		{
+			t = array[m];
+			array[m] = array[i];
+			array[i] = t;
+			print_array(array, size);
+		}
+	}
+}
