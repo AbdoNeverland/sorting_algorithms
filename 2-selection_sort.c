bddbbd2ec1985d1a74b1c314a1bfe8b14ad5ce7f
abdoneverland <ch.abdel.news@gmail.com>
2023-07-21 20:47:41 +0100

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,7 +7,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, t, c;
+	size_t i, j, t;
 	int m;
 
 	for (i = 0; i < size; i++)
@@ -16,9 +16,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i; j < size; j++)
 		{
 			if (array[j] < array[m])
-			{
 				m = j;
-			}
 		}
 		if (m != i)
 		{
